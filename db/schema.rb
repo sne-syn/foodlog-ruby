f8d12941576d99c9b17e2bd43990844d6514874b
sne-syn <snezhana.syngur@gmail.com>
2021-03-14 15:49:00 +0000

    Use postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,11 @@
 
 ActiveRecord::Schema.define(version: 2021_03_06_135738) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "entries", force: :cascade do |t|
-    t.string "meal_type"
+    t.text "meal_type"
     t.integer "calories"
     t.integer "proteins"
     t.integer "carbohydrates"
